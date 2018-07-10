7fb42594d9cf8c6937dee4cae7e40b9754303d76
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2018-07-10 08:08:36 -0600

    Worked with fetch() and added output div to index.html

--- javascript-practice/app.js
+++ javascript-practice/app.js
@@ -34,10 +34,10 @@ names.forEach((item, index) => console.log(index, item));
 2 - han
 3 - Chewie
 */
-
+/*
 var characters = ['Luke', 'Leia', 'Han', 'Chewie'];
 console.log(characters);
-/*
+
 characters.splice(0, 1,'Rey');
 console.log(characters);
 
@@ -60,7 +60,7 @@ console.log(characters);
 characters.sort();
 console.log(characters);
 
-*/
+
 
 let position = characters.indexOf('Kenny');
 console.log(position);
@@ -70,6 +70,46 @@ console.log(position);
 
 position = characters.lastIndexOf('Han');
 console.log(position);
+*/
+
+
+//basic fetch
+//using jsonplaceholder for the data
+//Remember that fetch returns a promise
+//HTTP status codes - http://www.restapitutorial.com/httpstatuscodes.html
+//to test with NODE we need to install the node-fetch package
+//  npm install node-fetch
+//let fetch = require('node-fetch');
+
+
+//get the details for a random user
+const root = 'http://jsonplaceholder.typicode.com';
+let id = Math.floor(Math.random() * 20) + 1;  //id 1 to 20
+let uri = root + '/users/' + id;
+
+console.log('FETCH: ', uri);
+//any user id higher than 10 will generate a 404 error
+
+fetch(uri)
+    .then(function(response){
+        if(response.status == 200){
+            return response.json();
+        } else {
+            throw new Error('Invalid user ID');
+        }
+        
+    })
+    .then((data) => {
+        console.log(data);
+        let jsonData = JSON.stringify(data);
+
+        let output = document.querySelector('.output');
+        output.textContent = jsonData;
+        console.log(data);
+    })
+    .catch( (err) => {
+        console.log('ERROR: ', err.message);
+    });
 
 
 
